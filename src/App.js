478dc233c64982043b84774dad1a33a2490377c7
ej-yotpo <161460939+ej-yotpo@users.noreply.github.com>
2024-12-11 23:49:59 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   let InstanceId_ReviewsTab = "";
 
   //Product ID
-  let ProductId = "";
+  let ProductId = "f7ed8289-a4c1-42cc-9374-09588df5d635";
 
   //Clipboard text here.
   let ClipBoardHeading = "Follow these steps to add this";
